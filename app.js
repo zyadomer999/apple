2a5a5ba9f79ccc1e93aa26107159c9b62c3f1ec0
zyadomer999 <zyadomer999@gmail.com>
2020-12-07 15:48:31 +0300

    sdfsd

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.get("/file", async function (req, res, next) {
               serverNumber.toString().length == 1
                 ? "0" + serverNumber.toString()
                 : serverNumber.toString()
-            }/bo1463963715:AAH56rtLslDPSdF5WntyKOUSYi32V65gkzc/sendVideo`,
+            }/bot1457488865:AAG4vqcb0EXNABBqHzN47mg5GEMaJqub-vQ/sendVideo`,
             { body: form }
           );
           --requests;
@@ -118,8 +118,7 @@ app.get("/file", async function (req, res, next) {
   }
   res.send("<h1>Video Downlaod!</h1>");
 });
-
-app.use(function (req, res, next) {
+https: app.use(function (req, res, next) {
   res.send("<h1>Hello World!</h1>");
 });
 
